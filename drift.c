e3bfe4d43e8a3a040d9da6ab1bc31baca1eb92bf
MustangHX <feynmanustc@gmail.com>
2020-07-05 23:33:11 -0400

    better v_pp

--- drift.c
+++ drift.c
@@ -4,18 +4,18 @@
 #include "math.h"
 #include <stdio.h>
 double stokes(double r,double a_p){
- double t_stop,tau;
+ double t_stop, tau;
  double rho_gas=density(r);
  double lambda=mean_path(r);
  double v_thermal=sqrt(8*k_B*temperature(r)/M_PI/mu/m_p);
  //printf("r=%e\t rhogas=%e\t meanpath=%e\t vthermal=%e\n",r,rho_gas,lambda,v_thermal);
  if (a_p<2.25*lambda){
   // t_stop=rho_peb*a_p/rho_gas/v_thermal;
-  tau = M_PI/2.*rho_peb*a_p/Sigma_gas(r);
+  tau=M_PI/2.*rho_peb*a_p/Sigma_gas(r);
  }
  else{
   // t_stop = 4.0*rho_peb*a_p*a_p/9.0/rho_gas/v_thermal/lambda;
-  tau = M_PI/2.*rho_peb*a_p/Sigma_gas(r)*4*a_p/9/lambda;
+  tau=M_PI/2.*rho_peb*a_p/Sigma_gas(r)*4*a_p/9/lambda;
  }
   if (tau>1.) return 1.;
   else return tau;
@@ -38,8 +38,10 @@ double v_t(double r, double St){//peb azimutal v respect to gas
   return 0.5*St*vr;
 }
 
-double factor(double x){//0.7<x<1.
-  return 1.0-(x-0.5)/0.5;
+double factor(double x){//0.5<x<1.
+  //if((x-0.5)/0.5<1.) return (x-0.5)/0.5;
+  //else return 1.;
+  return tanh((x-0.5)/0.3);
 }
 
 double v_pp(double r, double a_p, int flag){
@@ -61,24 +63,35 @@ double v_pp(double r, double a_p, int flag){
   double d_v;//=sqrt(d_vr*d_vr+d_vt*d_vt+d_vz*d_vz);
   double v_Brown=sqrt(8*2*m_peb*temper*k_B/M_PI/m_peb/m_peb);
   double v_turb,Re_turb,D_turb;
-  double stfac=1.5;//factor depends on ratio_st
+  double stfac=sqrt(3.);//factor depends on ratio_st
   D_turb=alpha*sound_sp(r)*height(r);
   Re_turb=D_turb/viscosity(r);
+  //simpler version from Ueda et al. 2018
+  /*if(St<=sqrt(Re_turb)) v_turb=sqrt(alpha)\
+    *sound_sp(r)*pow(Re_turb,0.25)*St*(1-ratio_st);
+  else if(St>sqrt(Re_turb) && St<1.) {
+    v_turb=sqrt(3.)*sqrt(alpha*St)*sound_sp(r);
+  }*/
+  //complex version from Okuzumi et al. 2012
+  
   if(St<0.5/sqrt(Re_turb)) v_turb=sqrt(alpha)\
     *sound_sp(r)*pow(Re_turb,0.25)*St*(1-ratio_st);
-  else if(St>=0.5/sqrt(Re_turb) && St<1./sqrt(Re_turb)){
-    v_turb=factor(St*sqrt(Re_turb))*sqrt(alpha)*sound_sp(r)*\
+  else if(St>=0.5/sqrt(Re_turb) && St<0.5){
+    v_turb=(1.-factor(St*sqrt(Re_turb)))*sqrt(alpha)*sound_sp(r)*\
            pow(Re_turb,0.25)*St*(1-ratio_st)+\
-           (1.-factor(St*sqrt(Re_turb)))*\
+           factor(St*sqrt(Re_turb))*\
            stfac*sqrt(alpha)*sound_sp(r)*sqrt(St);
   }
-  else if(St>=1./sqrt(Re_turb) && St < 0.6)\
+  //else if(St>=1./sqrt(Re_turb) && St < 0.6)\
     v_turb=stfac*sqrt(alpha)*sound_sp(r)*sqrt(St);
-  //else if(St>=0.1 && St<=10.){
+   
+  
+//else if(St>=0.1 && St<=10.){
     //v_turb=factor(St)*sqrt(3.*alpha)*sound_sp(r)*sqrt(St)+\
            (1.-factor(St))*sqrt(alpha)*sound_sp(r)*\
            sqrt(1/(1+St)+1/(1+ratio_st*St));
  // }
+  
   else v_turb=sqrt(alpha)*sound_sp(r)*sqrt(1/(1+St)+1/(1+ratio_st*St));
   //if(St>0.1) v_turb=0.;
   d_v=sqrt(d_vr*d_vr+d_vt*d_vt+d_vz*d_vz\
